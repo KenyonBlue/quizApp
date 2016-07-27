c43d2969afe5235fac7abe35fd6bc7ad357713bc
KenyonBlue <bluekenyfleo@gmail.com>
2016-07-26 23:16:29 -0400

    non interactive version of the quiz app

---
+++ app.js
@@ -0,0 +1,85 @@
+/*--- Step 1 - Defining global variables ---*/
+
+var questionsArray = [
+    //Question 1
+    {
+        questionText: 'What is a quality of pre-competitive affect?',
+        questionChoices: ['Low tension', 'Low depression', 'both'],
+        questionCorrectChoice: 2,
+        correctDetails: 'both low tension, and low depression are qualities of pre-competitive affect. '
+    },
+
+    //Question 2
+    {
+        questionText: 'What is a quality of emotional control?',
+        questionChoices: ['preforming under pressure', 'getting fustrated with results', 'blaming others'],
+        questionCorrectChoice: 0,
+        correctDetails: 'performing under pressure is a staple to emotional control.'
+    },
+
+    //Question 3
+    {
+        questionText: 'what is a quality of emotional control?',
+        questionChoices: ['having a wide array of emotions to both negetive and positive events', 'Responding to success and failure rationally', 'getting very excited with success'],
+        questionCorrectChoice: 1,
+        correctDetails: 'After a favorable big play reasonably celebrate with teammates After a negative play, listen and learn from mistakes.'
+    },
+
+    //Question 4
+    {
+        questionText: 'How fast is the fastest land speed record for cars?',
+        questionChoices: ['593 mph', '843 mph', '763 mph'],
+        questionCorrectChoice: 2,
+        correctDetails: 'The official land-speed record (measured over one mile) is 1,227.985 km/h (763.035 mi/h) by Thrust SSC.'
+    },
+
+    //Question 5
+    {
+        questionText: 'How many car companies are there in the world?',
+        questionChoices: ['Around 1500', 'Around 2500', 'Around 3500'],
+        questionCorrectChoice: 2,
+        correctDetails: 'There are more than 3500 Car Manufacturers worldwide.'
+    },
+
+    //Question 6
+    {
+        questionText: 'How much does an average person drive in a year?',
+        questionChoices: ['About 13000 miles', 'About 15000 miles', 'About 20000 miles'],
+        questionCorrectChoice: 0,
+        correctDetails: 'About 13000 miles.'
+    },
+
+    //Question 7
+    {
+        questionText: 'How much does the average person in USA spend on gas each year?',
+        questionChoices: ['$3400', '$1000', '$1300'],
+        questionCorrectChoice: 2,
+        correctDetails: 'About $1300.'
+    },
+
+    //Question 8
+    {
+        questionText: 'What is the best selling car in the world?',
+        questionChoices: ['Volkswagen Beetle', 'Toyota Corolla', 'Ford Model T'],
+        questionCorrectChoice: 1,
+        correctDetails: 'Toyota Corolla sold 40000 cars worldwide.'
+    },
+
+    //Question 9
+    {
+        questionText: 'How long is the longest car in the world?',
+        questionChoices: ['10feet', '65feet', '100 feet'],
+        questionCorrectChoice: 2,
+        correctDetails: 'A 30.5 m (100 ft) long 26-wheeled limousine was designed by Jay Ohrberg of Burbank, California, USA.'
+    },
+
+    //Question 10
+    {
+        questionText: 'What is price of the cheapest car in the world?',
+        questionChoices: ['$1000', '$3000', '$2000'],
+        questionCorrectChoice: 2,
+        correctDetails: 'The Tata Nano is a city car manufactured in India with a price of US$2000 new.'
+    }
+];
+/*--- Step 2 - Defining functions ---*/
+/*--- Step 3 - Using functions ---*/

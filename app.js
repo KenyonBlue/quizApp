cc58a5c77bc582532c3a21fe56cf43b6bc0679b3
KenyonBlue <bluekenyfleo@gmail.com>
2016-07-31 20:23:53 -0400

    added functionality.  quiz runs and try again button works

--- app.js
+++ app.js
@@ -35,20 +35,107 @@ var questionsArray = [
 
     //Question 5
     {
-        questionText: 'Low traint anxiety is also a key componennt to success.  what are low trait anxiety qualities successful individuals have?',
+        questionText: 'Low trait anxiety is also a key componennt to success.  what are low trait anxiety qualities successful individuals have?',
         questionChoices: ['Focused on task not the consequences', 'does not worry about things they cannot control', 'both answers are correct'],
         questionCorrectChoice: 2,
-        correctDetails: 'There are more than 3500 Car Manufacturers worldwide.'
+        correctDetails: 'Both answers are correct, successfull people usually posses low trait anxiety, which allows them to focus on task not consquences essentially only worrying about things they can control.'
     },
 
 
 ];
+
+var currentQuestionNumber = 0;
+var totalNumberOfQuestion = questionsArray.length;
+var totalNumberOfCorrectAnswers = 0;
 /*--- Step 2 - Defining functions ---*/
 // show multiple questions in quetion div
-function question() {
-    var question1 = document.getElementById('questionDisplay');
-    question1.addEventListener()
+function questionDisplay() {
+    //1 - update the each question text
+    $('#questiontitle').text(questionsArray[currentQuestionNumber].questionText);
+
+    //2 - display the what are the choices for the current question
+    //2.1 - first delete all the existing choices before populating it with new ones
+    $('#choices').empty();
+    //2.2 - then get the total number of choices for the current question
+    var totalNumberOfChoices = questionsArray[currentQuestionNumber].questionChoices.length;
+    //2.3 - loop through all the choices and append them to the choices container
+    for (i = 0; i < totalNumberOfChoices; i++) {
+        //2.3.1 - loop thru the answer choices and create a dynamically generated row for each of them
+        var buildEachChoiceHTML = "<input type='radio' class='option' name='option' value=" + i + ">" + questionsArray[currentQuestionNumber].questionChoices[i] + "<br>";
+        //2.3.2 append that row to the choices container in html
+        $('#choices').append(buildEachChoiceHTML);
+    }
+    //3 - displays the number of the current question
+    $('#questionNumberDisplay').text("Question " + (currentQuestionNumber + 1) + " of " + totalNumberOfQuestion);
 }
+
+$(document).ready(function () {
+    /*--- Hide quiz and result section on load ---*/
+    $('.explanation').hide();
+    $('.results').hide();
+
+    /*--- On start quiz ---*/
+    $('#startQuizButton').click(function () { //start the quiz and show the first question
+        $('.results').hide();
+        $('.explanation').hide();
+        $('.answer').show();
+        $('.start-section').hide();
+        //empty the result details container
+        $('#result_msg').empty();
+        questionDisplay();
+    });
+
+    /*--- Show quiz questions ---*/
+    $('.answer').on('click', '.option', function () {
+        //get the question answer from the user
+        var userAnswer = $("input[class='option']:checked").val();
+        //get the correct answer from the questionsArray above
+        var correctAnswer = questionsArray[currentQuestionNumber].questionCorrectChoice;
+        //compare the user answer with the correct answer
+        if (userAnswer == correctAnswer) {
+            //if the answer was correct increment the total number of correct answers
+            totalNumberOfCorrectAnswers++;
+            //console.log(totalNumberOfCorrectAnswers);
+        }
+        $('#result_msg').append("<h3>Q: " + questionsArray[currentQuestionNumber].questionText + "</h3>");
+        $('#result_msg').append("<h4>A: " + questionsArray[currentQuestionNumber].correctDetails + "</h4>");
+
+        //if quiz is finished, show result-section
+        if ((currentQuestionNumber + 1) == totalNumberOfQuestion) {
+
+            //show the final score
+            $('#finalScore').text(totalNumberOfCorrectAnswers + "/" + totalNumberOfQuestion);
+
+            //hide other containers
+            $('.question').hide();
+            $('.answer').hide();
+            $('.start-section').hide();
+            $('.counter').hide();
+            $('.result-section').show();
+        }
+        //else continue to next question
+        else {
+            //increment the current question number
+            currentQuestionNumber++;
+            //display the following question
+            questionDisplay();
+        }
+    });
+    /*--- Load the start section from the result section ---*/
+    $('.result-section').on('click', '#tryagain', function () {
+        $('.start-section').show();
+        $('.question').show();
+        $('.counter').show();
+        $('.quiz-section').hide();
+        $('.result-section').hide();
+
+        //reset variables to start quiz again
+        currentQuestionNumber = 0;
+        totalNumberOfCorrectAnswers = 0;
+    });
+});
+
+
 //create a intro and start button
 
 //show relitive answers to their repective question when they populate
